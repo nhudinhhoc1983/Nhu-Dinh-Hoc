c5d63090db5daa4b5703b79f35ecae67c4721f2c
nhudinhhoc1983 <106531058+nhudinhhoc1983@users.noreply.github.com>
2022-06-13 19:12:59 +0700

    bt nop

---
+++ bt13/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+int count;
+printf("\t This is a \n");
+for(count=100; count>=0; count-=5)
+{ printf("\n\t\t %d",count);
+}	
+printf("\n\t\t word.  \n");	
+	return 0;
+}
